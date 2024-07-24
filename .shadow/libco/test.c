a468d9048634f764a504ae037b4bbe27bdb36b2b
tracer-nju <tracer@nju.edu.cn>
2024-07-24 22:18:56 +0800

    Linux ubuntux86 6.7.11-orbstack-00143-ge6b82e26cd22 #1 SMP Sat Mar 30 12:20:36 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:18:56 up 16:03,  3 users,  load average: 0.11, 0.07, 0.02

--- .shadow/libco/test.c
+++ .shadow/libco/test.c
@@ -1,8 +1,12 @@
 #include "co.h"
+#include <stdio.h>
+void Print() {
+  for (int i = 0; i < 10; i++) {
+    printf("%d\n", i);
+  }
+}
 
-
-
-
-int main(){
-	return 0;
+int main() {
+  co_start("A", Print, NULL);
+  return 0;
 }
